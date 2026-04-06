32c93e4eca6eb0b6137e402b9525fa6e63e67508
Aniket-nitian <anik807621@gmail.com>
2026-04-06 20:56:36 +0530

    refactor comments for clarity in message_placeholder.py

--- PROMPTS_COMPONENT/message_placeholder.py
+++ PROMPTS_COMPONENT/message_placeholder.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-# chat template
+#? chat template
 chat_template = ChatPromptTemplate([
     ('system','You are a helpful customer support agent'),
     MessagesPlaceholder(variable_name='chat_history'),
@@ -7,13 +7,13 @@ chat_template = ChatPromptTemplate([
 ])
 
 chat_history = []
-# load chat history
+#? load chat history
 with open('chat_history.txt') as f:
     chat_history.extend(f.readlines())
 
 print(chat_history)
 
-# create prompt
+#? create prompt
 prompt = chat_template.invoke({'chat_history':chat_history, 'query':'Where is my refund'})
 
 print(prompt)
